23df4bfa97865043649459d4f06b5e0a13442122
huynhvantam <huynhvantamm@gmail.com>
2019-09-03 22:31:28 +0700

    a

---
+++ BasicExercises/coban1/Class27TongsoTrongchuoiso.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coban1
+{
+    class Class27
+    {
+        public static void Main()
+        {
+            Console.Write("Input  a number(integer): ");
+            //int n = int.Parse(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            //Console.WriteLine(n % 10);
+            //Console.WriteLine(n /= 10);
+            while (n != 0)
+
+            {
+                sum += n % 10;
+                n /= 10;
+                Console.WriteLine("Sum of the digits of the said integer: " + sum);
+            }
+
+        }
+    }
+
+}
+
