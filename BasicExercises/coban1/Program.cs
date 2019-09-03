af53d74928d6685c59b01615132a67a6ceb7b55f
huynhvantam <huynhvantamm@gmail.com>
2019-09-03 10:50:10 +0700

    a

---
+++ BasicExercises/coban1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coban1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
