89e01c966e3c4a2f00ed4ef7cb7ddd5d08d7bcff
huynhvantam <huynhvantamm@gmail.com>
2019-09-03 19:21:15 +0700

    a

---
+++ BasicExercises/coban1/Class23lowercase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coban1
+{
+    class Class23lowercase
+    {
+        public static void Main()
+        {
+            string str = "sdkfjSDFSDF";
+            Console.WriteLine(str.ToLower());
+        }
+    }
+}
---
+++ BasicExercises/coban1/Class24TimchuDainhatTrongchuoi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coban1
+{
+    class Class24TimchuDainhatTrongchuoi
+    {
+        public static void Main()
+        {
+            string line = "Write a C# Sharp Program to display the followingaa patteraaaaaan using the alphabet.";
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+
+            Console.WriteLine(word);
+
+        }
+    }
+}
