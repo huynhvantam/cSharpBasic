5a3a0d0276bce39a79b96f7fc29136747f059b89
huynhvantam <huynhvantamm@gmail.com>
2019-09-04 20:03:05 +0700

    aa

---
+++ OOT/ClassesStudent/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+class Person
+{
+    protected int age;
+
+    public void Greet()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+}
+
+class Student : Person
+{
+    public void ShowAge()
+    {
+        Console.WriteLine("My age is: {0} years old", age);
+    }
+}
+
+class Teacher : Person
+{
+    private string subject;
+
+    public void Explain()
+    {
+        Console.WriteLine("Explanation begins");
+    }
+}
+
+class StudentAndTeacherTest
+{
+    static void Main()
+    {
+        bool debug = false;
+
+        Person myPerson = new Person();
+        myPerson.Greet();
+
+        Student myStudent = new Student();
+        myStudent.SetAge(21);
+        myStudent.Greet();
+        myStudent.ShowAge();
+
+        Teacher myTeacher = new Teacher();
+        myTeacher.SetAge(30);
+        myTeacher.Greet();
+        myTeacher.Explain();
+
+        if (debug)
+            Console.ReadLine();
+    }
+}
