e05804487df1ac46b9358f452d944408b58d5d90
huynhvantam <huynhvantamm@gmail.com>
2019-09-05 23:33:07 +0700

    a

---
+++ BasicAlgorithm/Basic Algorithm/ConsoleApp1/Class38.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class38
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(new[] { 5, 5, 2 }));
+            Console.WriteLine(test(new[] { 5, 5, 2, 5, 5 }));
+            Console.WriteLine(test(new[] { 5, 6, 2, 9 }));
+            Console.ReadLine();
+        }
+        public static int test(int[] numbers)
+        {
+            var ctr = 0;
+            for (var i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i].Equals(5) && (numbers[i + 1].Equals(5) || numbers[i + 1].Equals(6))) ctr++;
+            }
+            return ctr;
+        }
+    }
+}
